4d1ae7d51b5be0bbd11397f52caab3039d16915e
ValentinusRendy <rendisatria28789@gmail.com>
2026-07-10 11:14:10 +0700

    add third pipeline (Wrong)

--- test_app.py
+++ test_app.py
@@ -5,9 +5,9 @@ from app import greet, add
 def test_greet():
       assert greet("Alice") == "Hello, Alice!"
       assert greet("Bob") == "Hello, Bob!"
-      
+
 
 def test_add():
-      assert add(2, 3) == 5
+      assert add(2, 3) == 100
       assert add(-1, 1) == 0
       assert add(0, 0) == 0
